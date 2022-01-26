54d810feb4bcbb1766e8a96580189f35c5708e51
zhaoyiping <zhaoyiping0622@gmail.com>
2022-01-26 15:10:31 +0000

    finish FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,52 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile name contents = 
+  do putStrLn ("============ " ++ name)
+     putStrLn contents
+  -- error "todo: Course.FileIO#printFile"
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles Nil = pure ()
+printFiles (x:.xs) = uncurry printFile x >> printFiles xs
+  -- error "todo: Course.FileIO#printFiles"
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = readFile path >>= (\x -> pure (path, x))
+  -- error "todo: Course.FileIO#getFile"
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = foldRight (\a b -> (:.) <$> getFile a <*> b) (pure Nil)
+  -- error "todo: Course.FileIO#getFiles"
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run name = do allLine <- readFile name
+              contents <- getFiles (lines allLine)
+              printFiles contents
+  -- error "todo: Course.FileIO#run"
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do files <- getArgs 
+          foldLeft (\b a -> run a >> b) (pure ()) files
+  -- error "todo: Course.FileIO#main"
 
 ----
 
